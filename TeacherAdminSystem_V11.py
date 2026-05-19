98aeb7cc8d07035ce2321d871d20979ca644cff9
MarieHung <129672929+MarieHung@users.noreply.github.com>
2026-05-19 13:43:10 +0800

    edit version number

--- TeacherAdminSystem_V11.py
+++ TeacherAdminSystem_V11.py
@@ -22,7 +22,7 @@ except ImportError:
 class TeacherStatsApp:
     def __init__(self, root):
         self.root = root
-        self.root.title("教師資料管理中心 - 行政綜合業務系統 (V9.4)")
+        self.root.title("教師資料管理中心 - 行政綜合業務系統 (11)")
         self.root.geometry("1300x900")
 
         # 全域資料變數
